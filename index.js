d74ca9503c07a05ffafdafb818eb87b22e12195a
Robert <95480383+robertiki2001@users.noreply.github.com>
2023-03-30 18:52:21 +0200

    desde cero

--- index.js
+++ index.js
@@ -9,32 +9,9 @@ app.use(express.static("public"));
 // Hola
 const io = new Server(httpServer, {});
 
-var idInterval = setInterval(enviar, 5000);
-
-function enviar() {
-    console.log("Enviant missatge")
-    io.emit("time",{"message": "Mañana hay examen"});
-}
-
-var usuaris = [];
-
 io.on("connection", (socket) => {
   
-    console.log('Connectat un client...')
-
-    socket.on("nickname", function(data) {
-            console.log(data.nickname)
-            
-            socket.data.nickname = data.nickname;
-            // respondre al que ha enviat
-            socket.emit("nickname rebut",{"response":"ok"})
-
-            // respondre a la resta de clients menys el que ha enviat
-            socket.broadcast.emit("nickname rebut",{"response":data.nickname});
-
-            // Totes les funcions disponibles les tenim a
-            //  https://socket.io/docs/v4/emit-cheatsheet/
-    })
+    
     socket.on("disconnect",function() {
         console.log("usuari desconnectat");
     })
--- public/main.js
+++ public/main.js
@@ -1,22 +1,3 @@
 
 const socket = io();
 
-const nicknameInput = document.getElementById("nicknameInput");
-const sendButton = document.getElementById("sendButton");
-sendButton.addEventListener("click", send)
-
-function send() {
-    socket.emit("nickname", {nickname: nicknameInput.value} )
-}
-
-socket.on('nickname rebut', function(data) {
-
-    console.log(data)
-
-})
-
-socket.on('time', function(data) {
-
-    console.log(data)
-
-})
